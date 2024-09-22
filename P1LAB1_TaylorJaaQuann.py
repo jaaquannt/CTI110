8c2b8d48fe9aac7a01d199c2fc7d1f38d2cfb831
jaaquannt <taylorj7260@student.faytechcc.edu>
2024-09-22 04:00:17 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_TaylorJaaQuann.py
@@ -0,0 +1,10 @@
+# JaaQuann Taylor
+# 9/22/2024
+# P1LAB1
+# Creating a prompt for the user's first and last name and then display a welcome message
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
